ae2be7ea91bdc878cb01868e3c00788a6f411809
Allen Hao <allen.hao@wustl.edu>
2016-09-25 00:36:48 -0500

    URL Scraper now dynamically checks if there are more players to scrape instead of relying on hard coded value

--- urlScraper.py
+++ urlScraper.py
@@ -1,15 +1,25 @@
 
 from bs4 import BeautifulSoup
-import urllib2
+import requests
+import re
+
 indexUrl = "http://www.2kmtcentral.com/17/players/page/"
 handle = open("playerLinks", "w")
-for x in range(0,34):
-        html = urllib2.urlopen(indexUrl + str(x));
+valid = True
+x = 33
+while(valid):
+        request = requests.get(indexUrl + str(x))
+        html = request.text;
         soup = BeautifulSoup(html, 'html.parser')
         content = soup.find_all( 'a', class_='name', href=True)
+        if len(content) == 0:
+            print "No more players"
+            valid = False
+            break
         # print content
         for a in content:
             url = a['href'].encode('utf-8') #Have to encode and decode because Nene has a circumflex in his name
             handle.write(url + "\n")
-        print "Done with " + str(x+1) + " out of 34 pages"
+        print "Done with " + str(x+1) + " pages"
+        x+=1
 handle.close()
