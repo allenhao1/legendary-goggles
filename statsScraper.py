6d668e9c553c29f3eee4b6b6670e1d031b06c047
Allen Hao <allen.hao@wustl.edu>
2016-09-24 23:36:06 -0500

    Getting ready to go off https://www.youtube.com/watch?v=xSLlZh9yelk

--- statsScraper.py
+++ statsScraper.py
@@ -5,30 +5,38 @@ from pymongo import MongoClient
 
 
 client = MongoClient('mongodb://localhost:27017/')
-url = "file:///Users/Allen/legendary-goggles/hgrant.html"
 db = client['2kdb']['players']
 with open('playerLinks') as f:
     links = f.readlines()
+url = "file:///Users/Allen/legendary-goggles/hgrant.html"
 
-for link in links:
-        link = link.replace("\n", "")
-        html = urllib2.urlopen(url);
-        _id = link.split("/")[5]
-        name = link.split("/")[6]
-        print _id + " "  + name
-playerObj = {}
 html = urllib2.urlopen(url);
 soup = BeautifulSoup(html, 'html.parser')
-playerObj["_id"] = url.split("/")[5]
-playerObj["name"] = url.split("/")[6]
-playerObj["overall"] = soup.find_all(class_="overall")[0].text
 
-#Look through blocks of attributes and parse out rating and name of rating
-for attributes in soup.find_all(class_="attribute-list"):
-    for attributes in attributes.find_all("li"):
-        stats = str(attributes.text)
 
-        statVal = int(stats[:2]) #Actual rating i.e. 89
-        statName = str(stats[3:]) #Type of stat i.e. contested 3
-        playerObj[statName] = statVal
-print playerObj
+
+for link in links:
+        try:
+            link = link.replace("\n", "")
+            playerObj = {}
+            html = urllib2.urlopen(link);
+            soup = BeautifulSoup(html, 'html.parser')
+
+            #Get initial data
+            playerObj["_id"] = link.split("/")[5]
+            playerObj["name"] = soup.find("table").find("td").text
+            playerObj["overall"] = soup.find_all(class_="overall")[0].text
+
+            Look through blocks of attributes and parse out rating and name of rating
+            for attributes in soup.find_all(class_="attribute-list"):
+                for attributes in attributes.find_all("li"):
+                    stats = str(attributes.text)
+
+                    statVal = int(stats[:2]) #Actual rating i.e. 89
+                    statName = str(stats[3:]) #Type of stat i.e. contested 3
+                    playerObj[statName] = statVal
+            db.insert_one(playerObj)
+            print playerObj["name"]
+        except Exception,e:
+            print str(e)
+            print link.split("/")[6] + " had an error" 
