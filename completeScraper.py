818c553eddd889f262b08ae19878be6dbf0e8235
Allen Hao <allen.hao@wustl.edu>
2017-02-26 14:43:47 -0600

    scraper

--- completeScraper.py
+++ completeScraper.py
@@ -10,34 +10,29 @@ import pymongo
 client = pymongo.MongoClient('mongodb://localhost:27017/')
 db = client['2kdb']['try2']
 
-##Both Nene's, 1016 Bosh and 2328 Kyrie don't work. Have to manually add
-# indexUrl = "http://www.2kmtcentral.com/17/players/page/"
-# playerLinks = []
-# valid = True
-# x = 0
+indexUrl = "http://www.2kmtcentral.com/17/players/page/"
+playerLinks = []
+valid = True
+x = 0
 playerErr = []
-# while(valid):
-#     request = requests.get(indexUrl + str(x))
-#     html = request.text;
-#     soup = BeautifulSoup(html, 'html.parser')
-#     content = soup.find_all( 'a', class_='name', href=True)
-#     if len(content) == 0:
-#         print "No more pages"
-#         valid = False
-#         break
-#     for a in content:
-#         url = a['href'].encode('utf-8') #Have to encode and decode because Nene has a circumflex in his name
-#         playerLinks.append(url)
-#     print "Done with " + str(x+1) + " pages"
-#     x+=1
-#     print x
-
-# playerLinks = open('playerLinks.txt', 'r')
-playerLinks = ['http://2kmtcentral.com/17/players/8027/adrian-dantley']
+while(valid):
+    request = requests.get(indexUrl + str(x))
+    html = request.text;
+    soup = BeautifulSoup(html, 'html.parser')
+    content = soup.find_all( 'a', class_='name', href=True)
+    if len(content) == 0:
+        print "No more pages"
+        valid = False
+        break
+    for a in content:
+        url = a['href'].encode('utf-8') #Have to encode and decode because Nene has a circumflex in his name
+        playerLinks.append(url)
+    print "Done with " + str(x+1) + " pages"
+    x+=1
+    print x
+totalPlayers = len(playerLinks)
 counter = 0
 playerObjList = []
-posErr = open("playerPosErr", "w")
-# totalPlayers = len(playerLinks)
 for link in playerLinks:
     try:
         html = requests.get(link).text
@@ -47,9 +42,7 @@ for link in playerLinks:
         playerObj["Name"] = str(soup.find("table").find("td").text.encode('UTF-8'))
         playerObj["Overall"] = int(soup.find_all(class_="overall")[0].text)
         pos = soup.find("span", "position-primary")
-        # print pos
         # # Onyx cards don't have position-primary. Have to check old school.
-        # # Modularize later
         if pos != None:
             playerObj["Position"] = pos.text
             secondary = soup.find("sup", "position-secondary")
@@ -79,23 +72,16 @@ for link in playerLinks:
             # Gotta account for on-ball d and pick & roll
             statName = str(re.search(r'\d\d((\s[^\+]+)+)', stats).group(1)).strip() #Type of stat i.e. contested 3
             playerObj[statName] = statVal
-        # db.insert_one(playerObj)
         print playerObj['Name']
         playerObjList.append(playerObj)
-        print playerObj
         counter +=1
         if counter % 100 == 0:
-            print str(counter) + " players added"
+            print str(counter) + " players added out of " + totalPlayers 
     except Exception, e:
         playerErr.append(link);
         print str(e)
         print sys.exc_traceback.tb_lineno
-        # print link.split("/")[6] + " had an error"
-        print link
 print str(counter) + " players added"
 if len(playerErr) == 0:
     for player in playerObjList:
         db.insert_one(player)
-for link in playerErr:
-    print link
-# playerLinks.close()
